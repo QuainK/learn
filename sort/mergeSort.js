66aef14e96e622524bb10af5c942ff8136a5dc2a
QuainK <1324575148@qq.com>
2023-03-12 15:49:43 +0800

    feat: 归并排序

---
+++ sort/mergeSort.js
@@ -0,0 +1,43 @@
+function mergeSort(arr1, arr2) {
+  let result = []
+
+  // 用两个指针，比较两个数组当前元素大小，取出较小的那个
+  let i = 0
+  let j = 0
+  for (let k = 0; k < arr1.length + arr2.length; k++) {
+    console.log('i =', i, 'j =', j)
+    // 如果其中一个数组已经全部取出
+    // 另一组剩下的一次性全部放进去
+    if (i >= arr1.length) {
+      while (j < arr2.length) {
+        result.push(arr2[j])
+        j++
+      }
+      break
+    } else if (j >= arr2.length) {
+      while (i < arr1.length) {
+        result.push(arr1[i])
+        i++
+      }
+      break
+    }
+
+    // 取出两个数组里较小的当前元素
+    if (arr1[i] <= arr2[j]) {
+      result.push(arr1[i])
+      if (i < arr1.length) i++
+    } else {
+      result.push(arr2[j])
+      if (j < arr2.length) j++
+    }
+
+    console.log('result', result)
+  }
+
+  return result
+}
+
+// let mergeArr1 = [1, 2, 3]
+let mergeArr1 = [1, 6, 9]
+let mergeArr2 = [1, 3, 5]
+console.log(mergeSort(mergeArr1, mergeArr2))
